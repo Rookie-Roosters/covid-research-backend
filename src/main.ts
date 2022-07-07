ab3c158fbea548db5ca0e2cb1114e70cf3187767
Manuel González Martínez <manuel.gonzalez.0518@gmail.com>
2022-07-06 20:06:00 -0500

    Project setup

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('COVID Research API')
+    .setDescription(
+      'COVID information API for researchers, health staff and general public',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+  console.log(`Server started at: ${await app.getUrl()}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
