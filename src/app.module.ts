779c84b15e54a7ad07c1ce2f7cd9962093142a88
Leonel Fernández <leonel-leonel-1@hotmail.com>
2022-07-19 20:00:46 -0500

    Solve authentication module problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseService } from '@config/database';
 import { UsersModule } from '@users/users.module';
 import { HttpExceptionFilter } from '@utils/filters';
 import { GlobalModule } from '@shared/global/global.module';
-import { AuthenticationModule } from '@authentication/authentication.module';
 import { ResearchesModule } from '@researches/researches.module';
 import { BookmarksModule } from '@bookmarks/bookmarks.module';
 import { CovidInfoModule } from './modules/covid-info/covid-info.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
 
 @Module({
     imports: [
